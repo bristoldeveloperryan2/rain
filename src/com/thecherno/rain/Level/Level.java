67f4ce62e72c2e4666ebe37f4107ad5a26f361a9
bristol.developer.ryan@gmail.com <bristol.developer.ryan@gmail.com>
2013-02-18 20:31:34 +0000

    Finished episode 35 - on to 36 https://www.youtube.com/watch?v=7gcxgmVXE80

--- src/com/thecherno/rain/Level/Level.java
+++ src/com/thecherno/rain/Level/Level.java
@@ -58,6 +58,12 @@ public class Level {
 		y0 = yScroll >> 4;
 		y1 = (yScroll + screen.height)>> 4; 
 		//end define render region
+		
+		for (int y = y0; y < y1; y++){
+			for (int x = x0; x < x1; x++){
+				getTile(x, y).render(x, y, screen);
+			}
+		}
 	}
 	
 	/**
